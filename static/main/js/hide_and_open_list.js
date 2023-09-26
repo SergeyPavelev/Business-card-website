f86065b93dca867982be1ea99c7d2e3c8d0b4375
SergeyPavelev <sergeypavelev141@gmail.com>
2023-09-26 07:53:43 +0500

    Changed the html file and the css file

--- static/main/js/hide_and_open_list.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('span').click(function () {
-        $('#list-navigation li:hidden').slice(0, 2).show();
-        if ($('#list-navigation li').length == $('#list-navigation li:visible').length) {
-            $('span ').hide();
-        }
-    });
-});
